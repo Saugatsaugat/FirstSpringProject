4773073b9e96e1e9913df048a9c74a8c799d0a6b
Saugat Thapa <saugatthapa2073@gmail.com>
2025-06-01 20:57:52 -0500

    IoC example added

--- src/main/java/com/saugat/firstspringproject/FirstSpringProjectApplication.java
+++ src/main/java/com/saugat/firstspringproject/FirstSpringProjectApplication.java
@@ -2,13 +2,15 @@ package com.saugat.firstspringproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FirstSpringProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FirstSpringProjectApplication.class, args);
-        System.out.println("Spring Boot project build above the Spring Framework.");
+        ApplicationContext context = SpringApplication.run(FirstSpringProjectApplication.class, args);
+        Student student = context.getBean(Student.class);
+        student.display();
     }
 
 }
---
+++ src/main/java/com/saugat/firstspringproject/Student.java
@@ -0,0 +1,10 @@
+package com.saugat.firstspringproject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+    public void display(){
+        System.out.println("Student class");
+    }
+}
