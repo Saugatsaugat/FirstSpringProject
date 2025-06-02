07e381cfc191a10ed1d31eb23ed2a26677e81922
Saugat Thapa <saugatthapa2073@gmail.com>
2025-06-01 21:34:44 -0500

    added example of aurowired annotation

---
+++ src/main/java/com/saugat/firstspringproject/Laptop.java
@@ -0,0 +1,11 @@
+package com.saugat.firstspringproject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void display(){
+        System.out.println("Laptop class");
+    }
+}
--- src/main/java/com/saugat/firstspringproject/Student.java
+++ src/main/java/com/saugat/firstspringproject/Student.java
@@ -1,10 +1,16 @@
 package com.saugat.firstspringproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Student {
+
+    @Autowired
+    Laptop laptop;
+
     public void display(){
-        System.out.println("Student class");
+        System.out.println("Student class is trying access Laptop class using Autowire...\n");
+        laptop.display();
     }
 }
