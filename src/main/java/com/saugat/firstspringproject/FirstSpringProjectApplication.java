085b5a14c310d507755ae42c32e0da660a0aaebe
Saugat Thapa <saugatthapa2073@gmail.com>
2025-06-04 23:38:13 -0500

    added example to demonstrate service layer using service tag

--- src/main/java/com/saugat/firstspringproject/FirstSpringProjectApplication.java
+++ src/main/java/com/saugat/firstspringproject/FirstSpringProjectApplication.java
@@ -1,6 +1,7 @@
 package com.saugat.firstspringproject;
 
 import com.saugat.firstspringproject.model.Student;
+import com.saugat.firstspringproject.service.LaptopService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +13,9 @@ public class FirstSpringProjectApplication {
         ApplicationContext context = SpringApplication.run(FirstSpringProjectApplication.class, args);
         Student student = context.getBean(Student.class);
         student.display();
+
+        LaptopService service = context.getBean(LaptopService.class);
+        service.laptopPerformance();
     }
 
 }
---
+++ src/main/java/com/saugat/firstspringproject/service/LaptopService.java
@@ -0,0 +1,11 @@
+package com.saugat.firstspringproject.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LaptopService {
+    public void laptopPerformance(){
+        System.out.println("This laptop is performing well.");
+    }
+
+}
